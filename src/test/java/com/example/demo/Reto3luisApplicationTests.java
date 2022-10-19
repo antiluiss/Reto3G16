ed3467d266b2aa5e8ffcdeff37c00733f084968e
antiluiss <luis.cediel.mt@usa.edu.co>
2022-10-18 19:40:45 -0500

    first commit

---
+++ src/test/java/com/example/demo/Reto3luisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3luisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
